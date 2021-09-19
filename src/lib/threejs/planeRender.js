170adc687e9cf5a75e21dfc90a3015ea0250b776
Konstantin Feuerborn <k.feuerborn@yahoo.de>
2021-09-19 13:32:55 +0200

    Add camera controls and refactor rendering

--- src/lib/threejs/planeRender.js
+++ src/lib/threejs/planeRender.js
@@ -1,32 +1,50 @@
 import * as THREE from 'three';
 
+/*
+    Some resources
+
+    https://threejs.org/examples/#webgl_loader_gltf
+*/
+
 import { MTLLoader } from 'src/lib/threejs/MTLLoader.js';
 import { OBJLoader } from 'src/lib/threejs/OBJLoader.js';
+import { OrbitControls } from 'src/lib/threejs/OrbitControls.js';
 
 export const init = (node) => {
-    const rect = node.getBoundingClientRect();
-    const width = rect.width;
-    const height = rect.height;
+    let camera, scene, renderer;
 
-    const scene = new THREE.Scene();
-    const camera = new THREE.PerspectiveCamera( 75, width / height, 0.1, 1000 );
-
-    const renderer = new THREE.WebGLRenderer();
-    renderer.setSize( width, height );
-    node.appendChild( renderer.domElement );
-
-    const geometry = new THREE.BoxGeometry();
-    const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-    const cube = new THREE.Mesh( geometry, material );
-    scene.add( cube );
-
-    camera.position.z = 5;
-
-    function animate() {
-        requestAnimationFrame( animate );
-        cube.rotation.x += 0.01;
-        cube.rotation.y += 0.01;
+    const render = () => {
         renderer.render( scene, camera );
     }
-    animate();
+
+    const init = () => {
+        const rect = node.getBoundingClientRect();
+        const width = rect.width;
+        const height = rect.height;
+
+        scene = new THREE.Scene();
+        camera = new THREE.PerspectiveCamera( 75, width / height, 0.1, 1000 );
+
+        renderer = new THREE.WebGLRenderer({ antialias: true });
+        renderer.setSize( width, height );
+        node.appendChild( renderer.domElement );
+
+        const geometry = new THREE.BoxGeometry();
+        const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+        const cube = new THREE.Mesh( geometry, material );
+        scene.add( cube );
+
+        camera.position.z = 5;
+
+        const controls = new OrbitControls( camera, renderer.domElement );
+        controls.addEventListener( 'change', render ); // use if there is no animation loop
+        controls.minDistance = 2;
+        controls.maxDistance = 10;
+        controls.target.set( 0, 0, - 0.2 );
+        controls.update();
+    }
+
+    init();
+    render();
+
 }
